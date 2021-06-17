9b19411b534e16a5bf75f07e3223eac864594fdd
mehulk123 <78738211+mehulk123@users.noreply.github.com>
2021-06-17 15:55:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("AJAV CRPTSI BECTOJ NOITNOTA ",700,140)
+    fill("lightblue")
+    text("Hint: JSON & API !!",700,170)
+
+    fill("white")
+    textSize(15)
+    text("SONCCRUTROT",100,240)
+    fill("lightblue")
+    text("Hint: USED IN CLASS !!",100,260)
+}
